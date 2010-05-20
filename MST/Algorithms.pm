e4cb17fc7a4c26678b13ff800cc06cc4830e7e8d
Guillaume-Jean Herbiet <guillaume-jean@herbiet.net>
2010-05-20 17:09:37 +0200

    In 'MST::Algorithms::dagrs' - fixed strict mode (was not testing neighbor node state) - fixed tree id update on mergers and splits - changed use of communities

--- MST/Algorithms.pm
+++ MST/Algorithms.pm
@@ -34,6 +34,7 @@ use List::Util 'shuffle';
 use Graph;
 
 use Community::Misc;
+use Community::Metrics;
 
 our $VERSION = '0.1';
 our @EXPORT  = qw(dagrs);
@@ -147,7 +148,9 @@ sub _dagrs_node {
                         my $tree1 = new Graph(undirected => 1);
                         foreach my $u (@cc1) {
                             $tree1->add_vertex($u);
+                            $G->set_vertex_attribute($u, "dagrs_treeid", $n);
                             foreach my $v (@cc1) {
+                                $G->set_vertex_attribute($v, "dagrs_treeid", $n);
                                 $tree1->add_vertex($v);
                                 if ($trees->{$t}->has_vertex($u, $v)) {
                                     $tree1->add_edge($u, $v);
@@ -160,9 +163,11 @@ sub _dagrs_node {
                         # Update the other subtree
                         my $tree2 = new Graph(undirected => 1);
                         foreach my $u (@cc2) {
-                            $tree1->add_vertex($u);
+                            $tree2->add_vertex($u);
+                            $G->set_vertex_attribute($u, "dagrs_treeid", $t);
                             foreach my $v (@cc2) {
                                 $tree2->add_vertex($v);
+                                $G->set_vertex_attribute($v, "dagrs_treeid", $t);
                                 if ($trees->{$t}->has_vertex($u, $v)) {
                                     $tree2->add_edge($u, $v);
                                 }
@@ -214,22 +219,96 @@ sub _dagrs_node {
         # Compute the total neigborhood similarity
         # w/o counting neighbors with which I have already merged
         #
-        my $n_sim_ttl = 0;
-        foreach my $nb ($G->neighbours($n)) {
-            if ($G->has_vertex_attribute($n, "n_sim-$nb") &&
-                (!$G->has_vertex_attribute($n, "dagrs_edgelabel-$nb") ||
-                ($G->has_vertex_attribute($n, "dagrs_edgelabel-$nb") &&
-                $G->get_vertex_attribute($n, "dagrs_edgelabel-$nb") == 0)) ) {
-                $n_sim_ttl += $G->get_vertex_attribute($n, "n_sim-$nb");
+        # my $n_sim_ttl = 0;
+        #         foreach my $nb ($G->neighbours($n)) {
+        #             if ($G->has_vertex_attribute($n, "n_sim-$nb") &&
+        #                 (!$G->has_vertex_attribute($n, "dagrs_edgelabel-$nb") ||
+        #                 ($G->has_vertex_attribute($n, "dagrs_edgelabel-$nb") &&
+        #                 $G->get_vertex_attribute($n, "dagrs_edgelabel-$nb") == 0)) ) {
+        #                 $n_sim_ttl += $G->get_vertex_attribute($n, "n_sim-$nb");
+        #             }
+        #         }
+        #         $n_sim_ttl = 1 if ($n_sim_ttl == 0);
+        
+        
+        #
+        # Node I will merge with
+        #
+        my $m; 
+        
+        #
+        # Do I hear about other trees in my community
+        #
+        my $other_trees = 0;
+        
+        #
+        # Search a possible "m" within neighbors of my community
+        #
+        my %C = community_assignment($G, %parameters);
+        
+        #print "($parameters{step}) $n: C(".get_node_community($G, $n, %parameters).")=";
+        #print join(' ', @{$C{get_node_community($G, $n, %parameters)}})."\n";
+        
+        R3C: foreach my $nb (@{$C{get_node_community($G, $n, %parameters)}}) {
+            #
+            # Check if I hear other tree ids
+            #
+            if ($G->has_edge($n, $nb) &&
+                $G->get_vertex_attribute($n, "dagrs_treeid") !=
+                $G->get_vertex_attribute($nb, "dagrs_treeid")) {
+                    $other_trees = 1;
+                    #print "($parameters{step}) $n: $nb has different tree id\n";
+                }
+            
+            if ( # This is really a neighbor (and not me)
+                $nb != $n && $G->has_edge($n, $nb) &&
+                # This neighbor also has token
+                $G->get_vertex_attribute($nb, "dagrs_label") eq "J" &&
+                # This neighbor hasn't been updated this step, unless we are not in strict mode
+                (!exists($parameters{strict}) || $G->get_vertex_attribute($nb, "dagrs_update") < $parameters{step})) {
+                
+                #print "($parameters{step}) $n: will merge with $nb\n";
+                $m = $nb;
+                last R3C;
             }
         }
-        $n_sim_ttl = 1 if ($n_sim_ttl == 0);
         
+        #
+        # If no "m" is found within my community, check that I dont hear about
+        # other trees in my community and search for a suitable node outside
+        # my community
+        #
+        if (!$m && $other_trees == 0) {
+            #print "($parameters{step}) $n: no mergeable node found in my community but no other tree heard\n";
+            
+            R3NC: foreach my $nb (shuffle($G->neighbours($n))) {
+                if ( # This is really not in my community
+                    get_node_community($G, $n, %parameters) != get_node_community($G, $nb, %parameters) &&
+                    # This neighbor also has token
+                    $G->get_vertex_attribute($nb, "dagrs_label") eq "J" &&
+                    # This neighbor hasn't been updated this step, unless we are not in strict mode
+                    (!exists($parameters{strict}) || $G->get_vertex_attribute($nb, "dagrs_update") < $parameters{step})) {
+
+                    #print "($parameters{step}) $n: will merge with $nb\n";
+                    $m = $nb;
+                    last R3NC;
+                }
+            }
+        }
+        elsif (!$m && $other_trees > 0) {
+            #print "($parameters{step}) $n: no mergeable node found in my community and other tree heard, skipping merge\n";
+        }
         
-        R3: foreach my $nb (shuffle($G->neighbours($n))) {
-            if ($G->get_vertex_attribute($nb, "dagrs_label") eq "J" &&
-                (!exists($parameters{strict}) ||
-                $G->get_vertex_attribute($n, "dagrs_update") < $parameters{step})) {
+        #
+        # If m is found, proceed to merge
+        #
+        if ($m) {
+            my $nb = $m;
+        
+        # R3: foreach my $nb (shuffle($G->neighbours($n))) {
+        #     if ($G->get_vertex_attribute($nb, "dagrs_label") eq "J" &&
+        #         (!exists($parameters{strict}) ||
+        #         $G->get_vertex_attribute($nb, "dagrs_update") < $parameters{step})) {
                 
                 #
                 # Test if the merge should be done :
@@ -239,79 +318,85 @@ sub _dagrs_node {
                 # added to a remainder which maked the merge more likely to
                 # happen next time.
                 #
-                my $rd = rand();
-                my $n_sim = 1;
-                $n_sim = $G->get_vertex_attribute($n, "n_sim-$nb")
-                    if ($G->has_vertex_attribute($n, "n_sim-$nb"));
-                $n_sim += $G->get_vertex_attribute($n, "remainder-$nb")
-                    if ($G->has_vertex_attribute($n, "remainder-$nb"));
-                $n_sim = 0.05 if ($n_sim == 0);
+                # my $rd = rand();
+                # my $n_sim = 1;
+                # $n_sim = $G->get_vertex_attribute($n, "n_sim-$nb")
+                #     if ($G->has_vertex_attribute($n, "n_sim-$nb"));
+                # $n_sim += $G->get_vertex_attribute($n, "remainder-$nb")
+                #     if ($G->has_vertex_attribute($n, "remainder-$nb"));
+                # $n_sim = 0.05 if ($n_sim == 0);
                 
                 #print "($n,$nb) n_sim = $n_sim, n_sim_ttl = $n_sim_ttl, rand = $rd\n";
                 
-                if ($rd > $n_sim / $n_sim_ttl) {
-                    #print "($n,$nb) skipping merge\n";
-                    #
-                    # Increment the remainder for this edge
-                    #
-                    #my $remainder = 0;
-                    #$remainder = $G->get_vertex_attribute($n, "remainder-$nb")
-                    #    if ($G->has_vertex_attribute($n, "remainder-$nb"));
-                    $G->set_vertex_attribute($n, "remainder-$nb", $n_sim);
-                    next R3;
-                }
-                
-                foreach my $n2 ($G->neighbours($n)) {
-                    $G->set_vertex_attribute($n, "remainder-$n2", 0);
-                }
+                # if ($rd > $n_sim / $n_sim_ttl) {
+                #     #print "($n,$nb) skipping merge\n";
+                #     #
+                #     # Increment the remainder for this edge
+                #     #
+                #     #my $remainder = 0;
+                #     #$remainder = $G->get_vertex_attribute($n, "remainder-$nb")
+                #     #    if ($G->has_vertex_attribute($n, "remainder-$nb"));
+                #     $G->set_vertex_attribute($n, "remainder-$nb", $n_sim);
+                #     next R3;
+                # }
+                # 
+                # foreach my $n2 ($G->neighbours($n)) {
+                #     $G->set_vertex_attribute($n, "remainder-$n2", 0);
+                # }
                 #print "($n,$nb) doing merge\n";
                 #
                 # End of merge test
                 #
                 
-                $G->set_vertex_attribute($n, "dagrs_edgelabel-$nb", 2);
-                $G->set_vertex_attribute($nb, "dagrs_edgelabel-$n", 1);
-                $G->set_vertex_attribute($nb, "dagrs_label", "N");
-                
-                if (exists($parameters{strict})) {
-                    $G->set_vertex_attribute($n, "dagrs_update", $parameters{step});
-                    $G->set_vertex_attribute($nb, "dagrs_update", $parameters{step});
-                }
-                
-                # Merge the two trees
-                my $d;
-                foreach my $t1 (keys %{$trees}) {
-                    if ($trees->{$t1}->has_vertex($n)) {
-                        #print "Tree ".$trees->{$t1}." has vertex $n\n";
-                        $trees->{$t1}->add_edge($n, $nb);
-                        #print $trees->{$t1}."\n";
-                        foreach my $t2 (keys %{$trees}) {
-                            if ($trees->{$t2}->has_vertex($nb) && $t2 != $t1) {
-                                #print "Tree ".$trees->{$t1}." has vertex $nb\n";
-                                foreach my $v ($trees->{$t2}->vertices) {
-                                    $trees->{$t1}->add_vertex($v);
-                                }
-                                foreach my $e ($trees->{$t2}->edges) {
-                                    my ($u, $v) = @{$e};
-                                    $trees->{$t1}->add_edge($u, $v);
-                                }
-                                #print $trees->{$t1}."\n";
-                                $d = $t2;
-                            }
-                        }
-                    }
+            $G->set_vertex_attribute($n, "dagrs_edgelabel-$nb", 2);
+            $G->set_vertex_attribute($nb, "dagrs_edgelabel-$n", 1);
+            $G->set_vertex_attribute($nb, "dagrs_label", "N");
+            
+            if (exists($parameters{strict})) {
+                $G->set_vertex_attribute($n, "dagrs_update", $parameters{step});
+                $G->set_vertex_attribute($nb, "dagrs_update", $parameters{step});
+            }
+            
+            # Merge the two trees
+            my $d;
+            foreach my $t1 (keys %{$trees}) {
+                if ($trees->{$t1}->has_vertex($n)) {
+                    #print "Tree ".$trees->{$t1}." has vertex $n\n";
+                    $trees->{$t1}->add_edge($n, $nb);
                     $G->set_vertex_attribute($n, "dagrs_treeid", $t1);
                     $G->set_vertex_attribute($nb, "dagrs_treeid", $t1);
+                    #print $trees->{$t1}."\n";
+                    foreach my $t2 (keys %{$trees}) {
+                        if ($trees->{$t2}->has_vertex($nb) && $t2 != $t1) {
+                            #print "Tree ".$trees->{$t1}." has vertex $nb\n";
+                            foreach my $v ($trees->{$t2}->vertices) {
+                                $trees->{$t1}->add_vertex($v);
+                                $G->set_vertex_attribute($v, "dagrs_treeid", $t1);
+                            }
+                            foreach my $e ($trees->{$t2}->edges) {
+                                my ($u, $v) = @{$e};
+                                $G->set_vertex_attribute($u, "dagrs_treeid", $t1);
+                                $G->set_vertex_attribute($v, "dagrs_treeid", $t1);
+                                $trees->{$t1}->add_edge($u, $v);
+                            }
+                            #print $trees->{$t1}."\n";
+                            $d = $t2;
+                        }
+                    }
                 }
-                #print "Deleting tree ".$trees->{$d}."that contained vertex $nb\n" if ($d);
-                delete($trees->{$d}) if ($d);
-                
-                $updated = 1;
-                $op++;            
-                #print "$n: R3 ($nb)\n";
-                last R3;
-                
             }
+            #print "Deleting tree ".$trees->{$d}."that contained vertex $nb\n" if ($d);
+            delete($trees->{$d}) if ($d);
+            
+            $updated = 1;
+            $op++;            
+                #print "$n: R3 ($nb)\n";
+                #last R3;
+                
+            #}
+        }
+        else {
+            #print "($parameters{step}) $n: no mergeable node found in my neighborhood\n";
         }
     }
     #
@@ -324,7 +409,7 @@ sub _dagrs_node {
                 $G->has_vertex_attribute($n, "dagrs_edgelabel-$nb") &&
                 $G->get_vertex_attribute($n, "dagrs_edgelabel-$nb") > 0 &&
                 (!exists($parameters{strict}) ||
-                $G->get_vertex_attribute($n, "dagrs_update") < $parameters{step})) {
+                $G->get_vertex_attribute($nb, "dagrs_update") < $parameters{step})) {
                 
                 $G->set_vertex_attribute($n, "dagrs_label", "N");
                 $G->set_vertex_attribute($nb, "dagrs_label", "J");
